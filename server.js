0709c2486757bf363ee48ed4d9c6035114515ffd
lexinor <alternonolimit@gmail.com>
2020-02-03 17:33:16 +0100

    Added first api route to get matches for a league

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const request = require('request')
+const key = require('./key.js')
+
+
+const API_KEY = key.getKey()
+const PORT = 3000;
 
 var app = express()
 
@@ -7,4 +12,31 @@ app.get('/', (req,res) => {
     res.send("Hello World")
 })
 
-app.listen(3000)
+app.get('/league/:leagueId', (req, res) => {
+    var leagueId = req.params.leagueId;
+    console.log(leagueId);
+
+    res.send(leagueId)
+})
+
+app.get('/match/:leagueId/:date', (req, res) => {
+    var leagueId = req.params.leagueId
+    var date = req.params.date
+    
+    let options = { url: "https://api-football-v1.p.rapidapi.com/v2/fixtures/league/"+ leagueId + "/" + date, headers: {'X-RapidAPI-Key':  API_KEY} };
+
+    
+    request(options,(err,response,body) => {
+
+        let matches = JSON.parse(body);
+        //let imgHint = matches['indice'];
+        //singularImage = singularImage['nomImg'];
+
+        res.send(body)      
+        
+    });
+})
+
+app.listen(PORT, () => {
+    console.log("Server stared at port => " + PORT)
+})
